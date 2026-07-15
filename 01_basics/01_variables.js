3c626c20c696ca9c56b29febd7f39aeaa1e8a1fe
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-15 15:22:33 +0000

    Update README with Day 1 progress

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 845
+let accountEmail = "jaidmulla@google.com"
+var accountPass = "12344"
+let accountState;
+accountCity = "Sangli"
+accountEmail = "jaid@123"
+accountPass = "34234"
+accountCity = "Ashta"
+
+
+// accountId = 3 // not allowed
+console.table([accountEmail,accountId,accountPass,accountCity,accountState])
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # js-hindi-yt
 A code repo for javascript series at yt channel 
+
+// date and point learned..
+15-07-26: test and varables
