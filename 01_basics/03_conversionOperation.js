1cf76a292174ffe196075c55cbc30832d6738ea2
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-16 05:29:15 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = 66
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+
+//"33" =>  33
+//"33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = "jaid"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1==> true; 0=> false;
+//"" => false
+//"jaid" => ture
+
+//converting number into string
+
+let sumNumber = 87
+
+let StringNumber = String(sumNumber);
+console.log(StringNumber);
+console.log(typeof StringNumber);
