9997c050829b9313dc6b036ad8c88d4088be1a9d
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-16 06:07:59 +0000

    conversion of operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,4 +1,4 @@
-let score = 66
+// let score = 66
 
 // console.log(typeof score)
 
@@ -9,18 +9,44 @@ let score = 66
 //"33abc" => NaN
 //true => 1; false => 0
 
-let isLoggedIn = "jaid"
+// let isLoggedIn = "jaid"
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 //1==> true; 0=> false;
 //"" => false
 //"jaid" => ture
 
 //converting number into string
 
-let sumNumber = 87
+// let sumNumber = 87
 
-let StringNumber = String(sumNumber);
-console.log(StringNumber);
-console.log(typeof StringNumber);
+// let StringNumber = String(sumNumber);
+// console.log(StringNumber);
+// console.log(typeof StringNumber);
+
+
+//******Operations*******
+
+// let value = 3
+// let negValue = -value
+// // console.log(-value)
+
+//Connect String
+
+// let str1 = "Jaid"
+// let str2 = " Mulla"
+// let str3 = str1+str2
+// console.log(str3);
+
+// let num1, num2, num3
+
+// num2 = num2 = num3 = 4  //Not a Good method of writing a code use readable format
+
+let gameCounter = 100;
+gameCounter++;  //Increment operator
+console.log(gameCounter);
+
+
+//there are many operator in js such as 
+//Incrment, decrement, comparesion, logical etc.
