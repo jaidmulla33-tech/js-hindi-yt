05c45b8f76f9afcd3564abbcb8572b76825ac183
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-16 05:12:01 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3)  we are using node js not browser
+
+console.log(7+7);
+
+console.log("Jaid Mulla")
+
+//Data types
+
+let name = "jaid"
+let age = 20  //in 2026
+let isLoggedIn = false
+console.table([name, age, isLoggedIn])
+
+
+// number => 2 to power 50
+// brigint
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => variable is defined but not provided ant value
+//symbol => unique
+
+//object
+
+console.log(typeof"Jaid") //use for knowing type of provided thing
+console.log(typeof null)
+console.log(typeof undefined)
