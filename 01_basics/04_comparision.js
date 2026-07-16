527457321fd49f43daebfceb51d050cf32790427
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-16 06:31:50 +0000

    learn comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,18 @@
+//Comparision operator
+
+console.log(5 < 10)
+console.log(10 > 5)
+console.log(2 == 2)f
+console.log(2 != 1)
+console.log(2 >= 1)
+console.log(4 <= 5)
+
+console.log(null > 0);
+console.log(null == 0);  // int these operation the js consider null as more than 0
+console.log(null >= 0);  // in these operation the js consider the value of null is 0
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined < 0);   
+//it return false because the js consider the undefined as more than zero 
+//We avoid such type operation
