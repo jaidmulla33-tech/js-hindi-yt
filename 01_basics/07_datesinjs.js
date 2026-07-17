60c372660a06c061eca06a63de2a57da528e641d
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-17 10:26:06 +0000

    remove comment

--- 01_basics/07_datesinjs.js
+++ 01_basics/07_datesinjs.js
@@ -1,25 +1,25 @@
-// //Date
+//Date
 
-// let myDate = new Date()
-// console.log(myDate.toString());
-// console.log(myDate.toISOString());
-// console.log(myDate.toLocaleDateString());
-// console.log(myDate.toLocaleTimeString());
-// console.log(myDate.getTimezoneOffset());
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toISOString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getTimezoneOffset());
 
-// console.log(typeof myDate);  //Date is an object
+console.log(typeof myDate);  //Date is an object
 
-// let myCreatedDate = new Date(2026, 6, 25)
-// console.log(myCreatedDate.toString());  // Month is start from 0 / convert into String fro readable format
+let myCreatedDate = new Date(2026, 6, 25)
+console.log(myCreatedDate.toString());  // Month is start from 0 / convert into String fro readable format
 
-// let cusDate = new Date("26-07-25")
-// console.log(myCreatedDate.toLocaleString());
+let cusDate = new Date("26-07-25")
+console.log(myCreatedDate.toLocaleString());
 
-//Time stamp
+Time stamp
 
-// let myTimeStamp = Date.now()
-// console.log(myTimeStamp);  //mili sce time from 1 jan 1970
-// console.log(myCreatedDate.getTime());
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);  //mili sce time from 1 jan 1970
+console.log(myCreatedDate.getTime());
 
 
 let newDate = new Date()
