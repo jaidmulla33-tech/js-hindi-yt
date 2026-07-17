5def50531563f31434bd8eb53b4b2e62293de470
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-17 10:24:47 +0000

    Learn Date and time

---
+++ 01_basics/07_datesinjs.js
@@ -0,0 +1,33 @@
+// //Date
+
+// let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getTimezoneOffset());
+
+// console.log(typeof myDate);  //Date is an object
+
+// let myCreatedDate = new Date(2026, 6, 25)
+// console.log(myCreatedDate.toString());  // Month is start from 0 / convert into String fro readable format
+
+// let cusDate = new Date("26-07-25")
+// console.log(myCreatedDate.toLocaleString());
+
+//Time stamp
+
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);  //mili sce time from 1 jan 1970
+// console.log(myCreatedDate.getTime());
+
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1); // to return current month
+
+
+newDate.toLocaleString('dfault',{
+    weekday:"long",
+    weekday:"narrow",
+})
