44e25a4cc593cd65363e03b8c4a06808a4a827aa
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-18 10:05:55 +0000

    remove comments

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -1,42 +1,42 @@
-// const appUser = new Object()
-// // const appUser = {}
+const appUser = new Object()
+// const appUser = {}
 
-// appUser.id = "123asd"
-// appUser.name = "Jaid"
-// appUser.isLoggedIn = false
+appUser.id = "123asd"
+appUser.name = "Jaid"
+appUser.isLoggedIn = false
 
-// console.log(appUser)
-
-
-// const regularUser = {
-//     email: "jaidmulla@",
-//     fullName: {
-//     userFullname :{
-//             firstName : "Jaid",
-//             lastName : "Mulla"
-//         }
-//     }   
-// }
-// console.log(regularUser.fullName.userFullname.firstName)   //? if you dont know data is avilable or not
+console.log(appUser)
 
 
-// // const obj1 = {1: "a", 2: "b"}
-// // const obj2 = {3: "c", 4: "d"} 
-// // const obj4 = {5: "e", 6: "f"}
-// // // const obj3 = {obj1, obj2} //To mearge two object
+const regularUser = {
+    email: "jaidmulla@",
+    fullName: {
+    userFullname :{
+            firstName : "Jaid",
+            lastName : "Mulla"
+        }
+    }   
+}
+console.log(regularUser.fullName.userFullname.firstName)   //? if you dont know data is avilable or not
 
-// // // const obj3 = Object.assign({},obj1, obj2, obj4)  //{} use of these is good practice
-// // //const obj3 = {...obj1,...obj2,...obj4}  //Most used and new syntax
-// // console.log(obj3);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"} 
+const obj4 = {5: "e", 6: "f"}
+// const obj3 = {obj1, obj2} //To mearge two object
+
+// const obj3 = Object.assign({},obj1, obj2, obj4)  //{} use of these is good practice
+//const obj3 = {...obj1,...obj2,...obj4}  //Most used and new syntax
+console.log(obj3);
 
 
-// console.log(appUser);
+console.log(appUser);
 
-// console.log(Object.keys(appUser));  //To accsess key eg. id, name isLoggedin
-// console.log(Object.values(appUser)); //To access the valule
-// //there are many methods we use
+console.log(Object.keys(appUser));  //To accsess key eg. id, name isLoggedin
+console.log(Object.values(appUser)); //To access the valule
+//there are many methods we use
 
-// console.log(appUser.hasOwnProperty('id'))  // ask the property it return boolean value
+console.log(appUser.hasOwnProperty('id'))  // ask the property it return boolean value
 
 
 const course = {
