51b1376dccc82024ff3e6effbd2036c18f826f08
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-22 11:24:58 +0000

    for of loop

--- 05_literations/three.js
+++ 05_literations/three.js
@@ -2,41 +2,41 @@
 
 const arr = [1, 2, 3, 4, 5, 6]
 
-// for (const num of arr) {
-//     console.log(num);
-// }
+for (const num of arr) {
+    console.log(num);
+}
 
 
-// const greating = "Hello Bhai"
-// for (const greet of greating) {
-//     console.log(`Each char is ${greet}`)
-// }
+const greating = "Hello Bhai"
+for (const greet of greating) {
+    console.log(`Each char is ${greet}`)
+}
 
 
 //Maps
 
-// const map = new Map ()               //Map is an object
-// map.set('IN', "Inaid");
-// map.set('FR', "France");
-// map.set('DU', "Dubai");
-// //console.log(map);
-// //Output =>   Map(3) { 'IN' => 'Inaid', 'FR' => 'France', 'DU' => 'Dubai' }
+const map = new Map ()               //Map is an object
+map.set('IN', "Inaid");
+map.set('FR', "France");
+map.set('DU', "Dubai");
+//console.log(map);
+//Output =>   Map(3) { 'IN' => 'Inaid', 'FR' => 'France', 'DU' => 'Dubai' }
 
-// for (const key of map) {
-//     console.log(key);
-// }
-// //Output =>  [ 'IN', 'Inaid' ]
-// //           [ 'FR', 'France' ]
-// //           [ 'DU', 'Dubai' ]
+for (const key of map) {
+    console.log(key);
+}
+//Output =>  [ 'IN', 'Inaid' ]
+//           [ 'FR', 'France' ]
+//           [ 'DU', 'Dubai' ]
 
 
 
-// for (const [key, value] of map) {   //Syntx for saperate the value and key from array
-//     console.log(key, ':-', value);
-// }
-// //Output =>  IN :- Inaid
-// //           FR :- France
-// //           DU :- Dubai
+for (const [key, value] of map) {   //Syntx for saperate the value and key from array
+    console.log(key, ':-', value);
+}
+//Output =>  IN :- Inaid
+//           FR :- France
+//           DU :- Dubai
 
 
 
