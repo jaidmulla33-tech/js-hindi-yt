4a4fb5ccb7019d88f2f1e9db3f4cd9e3947d401d
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-22 10:20:24 +0000

    while loop

---
+++ 05_literations/two.js
@@ -0,0 +1,33 @@
+//while loop
+
+// while( 2== 2){
+//     console.log("Conditon checked");
+//     break;
+// }
+
+// let index = 0;
+// while(index <= 10){
+//     console.log(`Value of index is ${index}`);
+//     index = index+2;
+// }
+
+
+// let myArray = ["Batman", "Superman", "Flash"]
+
+// let arr = 0;
+// while(arr < myArray.length){
+//     console.log(`Value is ${myArray[arr]}`)
+//     arr = arr+1;
+// }
+
+
+
+
+//do while loop
+
+let score = 1; 
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+} while (score <= 10);
