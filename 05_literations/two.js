e7414d23779db458f1c9e8535ee0612e7e13b6d8
Jaid Mulla <jaidmulla33@gmail.com>
2026-07-22 10:26:24 +0000

    while loop

--- 05_literations/two.js
+++ 05_literations/two.js
@@ -1,24 +1,24 @@
 //while loop
 
-// while( 2== 2){
-//     console.log("Conditon checked");
-//     break;
-// }
+while( 2== 2){
+    console.log("Conditon checked");
+    break;
+}
 
-// let index = 0;
-// while(index <= 10){
-//     console.log(`Value of index is ${index}`);
-//     index = index+2;
-// }
+let index = 0;
+while(index <= 10){
+    console.log(`Value of index is ${index}`);
+    index = index+2;
+}
 
 
-// let myArray = ["Batman", "Superman", "Flash"]
+let myArray = ["Batman", "Superman", "Flash"]
 
-// let arr = 0;
-// while(arr < myArray.length){
-//     console.log(`Value is ${myArray[arr]}`)
-//     arr = arr+1;
-// }
+let arr = 0;
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`)
+    arr = arr+1;
+}
 
 
 
